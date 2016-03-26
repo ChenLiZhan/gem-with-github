792b657ebe0f273ff0f4d142bc10a02a9ee3cee4
chenlizhan <chung1350@hotmail.com>
2016-03-26 14:59:53 +0800

    Exclude using concurrent

--- app.rb
+++ app.rb
@@ -1,16 +1,16 @@
 require 'gems'
 require 'csv'
 require 'json'
-require 'concurrent'
+# require 'concurrent'
 
 gem_list = []
-pool = Concurrent::FixedThreadPool.new(5)
-lock = Mutex.new
+# pool = Concurrent::FixedThreadPool.new(5)
+# lock = Mutex.new
 repo_regex = /https?:\/\/github.com\/([\w-]*)\/([\w-]*)\/?/
 
 CSV.foreach('./rubygems_list.csv') do |gem_name|
-  sleep 0.5
-  pool.post do
+  # sleep 0.5
+  # pool.post do
     begin
       gem_info = Gems.info gem_name[0]
       repo_user = ''
@@ -31,23 +31,23 @@ CSV.foreach('./rubygems_list.csv') do |gem_name|
       end
 
       if !repo_user.empty? && !repo_name.empty?
-        lock.synchronize {
+        # lock.synchronize {
           gem_list << {
             'gem_name'    => gem_name,
             'repo_user'   => repo_user,
             'repo_name'   => repo_name
           }
           puts "#{gem_name} ... DONE **** #{gem_list.length} in the array"
-        }
+        # }
       end
     rescue => error
       puts error
     end
-  end
+  # end
 end
 
-pool.shutdown
-pool.wait_for_termination
+# pool.shutdown
+# pool.wait_for_termination
 
 File.open('./gems.json', 'w') do |f|
   f.write(gem_list.to_json)
